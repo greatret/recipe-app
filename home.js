0a35bec4fdd5a970a8e044635a76e11e31b198f3
Rehan <rehankhan6711@gmail.com>
2021-02-11 13:13:56 +0530

    oqwiejqow

--- home.js
+++ home.js
@@ -192,9 +192,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".menu_bar").style.bottom = "20px";
+    document.querySelector(".menu_bar").style.bottom = "0px";
   } else {
-    document.querySelector(".menu_bar").style.bottom = "-50px";
+    document.querySelector(".menu_bar").style.bottom = "-25px";
   }
   prevScrollpos = currentScrollPos;
 }
